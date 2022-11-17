d8726fc3e1a5434eb70e8db2a2017b644e1c1bbf
Atabow <bharat8@hotmail.com>
2022-11-17 09:29:47 -0800

    Add files via upload

---
+++ agentdeployment.ps1
@@ -0,0 +1,23 @@
+param(
+    [Parameter(Mandatory=$true, HelpMessage='File name.')]
+    [ValidateNotNull()]
+    [string] $FileName,
+)
+
+$LogFilePath = "C:\CustomScriptExtension.{0}.log" -f [guid]::NewGuid().ToString()
+
+function WriteLog([string] $message)
+{
+    $date = Get-Date
+    $datedMessage = "$date : $message"
+    Add-Content -Path $LogFilePath -Value $datedMessage -Force
+}
+
+function WriteErrorAndExit([string] $errorMessage)
+{
+    WriteLog $errorMessage
+    Start-Sleep -Seconds 5
+    [Environment]::Exit(1)
+}
+
+WriteLog "Successfully executed the script!"
